ac2ef4c782e55df3966157e5530cff4acbeb6248
starseat <starseat3@gmail.com>
2021-09-27 03:24:00 +0900

    링크복사 처리 수정

--- 99.src/public/js/main.js
+++ 99.src/public/js/main.js
@@ -18,23 +18,47 @@ $(document).ready(function() {
     });
 });
 
-function copyToClipboard() {
-    var copyUrl = 'http://donmmelier.com/';
+// function copyToClipboard() {
+//     var copyUrl = 'http://donmmelier.com/';
 
-    var agent = navigator.userAgent.toLowerCase();
-    if ((navigator.appName == 'Netscape' && agent.indexOf('trident') != -1) || (agent.indexOf("msie") != -1)) {
-        // IE 용
-        window.clipboardData.setData("Text", copyUrl);
-    } else {
-        navigator.clipboard.writeText(copyUrl)
-            //     .then(() => {
-            //     console.log("Text copied to clipboard...")
-            // })
-            //     .catch(err => {
-            //     console.log('Something went wrong', err);
-            // })
-        ;
-    }
+//     var agent = navigator.userAgent.toLowerCase();
+//     if ((navigator.appName == 'Netscape' && agent.indexOf('trident') != -1) || (agent.indexOf("msie") != -1)) {
+//         // IE 용
+//         window.clipboardData.setData("Text", copyUrl);
+//     } else {
+//         navigator.clipboard.writeText(copyUrl)
+//             //     .then(() => {
+//             //     console.log("Text copied to clipboard...")
+//             // })
+//             //     .catch(err => {
+//             //     console.log('Something went wrong', err);
+//             // })
+//         ;
+//     }
+
+//     alert('링크가 복사되었습니다.');
+// }
+
+function copyToClipboard(e) {
+    var tempItem = document.createElement('input');
+
+    tempItem.setAttribute('type', 'text');
+    tempItem.setAttribute('display', 'none');
+
+    // let content = e;    
+    // if (e instanceof HTMLElement) {
+    //     content = e.innerHTML;
+    // }
+
+    let content = 'http://donmmelier.com/';
+
+    tempItem.setAttribute('value', content);
+    document.body.appendChild(tempItem);
+
+    tempItem.select();
+    document.execCommand('Copy');
+
+    tempItem.parentElement.removeChild(tempItem);
 
     alert('링크가 복사되었습니다.');
 }
